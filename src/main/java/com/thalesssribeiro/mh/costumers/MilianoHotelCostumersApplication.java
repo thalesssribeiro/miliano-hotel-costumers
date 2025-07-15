8ec1fb8e1871338ed6eeb204f4949f6cae5b7f1a
thalesssribeiro <thalesmaxi@gmail.com>
2025-07-14 21:32:06 -0300

    Primeiro passo

---
+++ src/main/java/com/thalesssribeiro/mh/costumers/MilianoHotelCostumersApplication.java
@@ -0,0 +1,13 @@
+package com.thalesssribeiro.mh.costumers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilianoHotelCostumersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilianoHotelCostumersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalesssribeiro/mh/costumers/MilianoHotelCostumersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesssribeiro.mh.costumers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilianoHotelCostumersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
